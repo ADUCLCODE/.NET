40244c290ff0c3ec2e13cec06fdb210031599c07
iDIDIT <aduclase@gmail.com>
2023-04-11 10:31:35 +0200

    1st update

--- Ejercicios/Ejercicios/Program.cs
+++ Ejercicios/Ejercicios/Program.cs
@@ -1,15 +1,31 @@
-﻿using System;
+﻿using Ejercicios.TresEnRaya;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Ejercicios
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Menu de Ejercicios");
+            Console.WriteLine("Seleccione el ejercicio al que quiere acceder");
+
+            string response = Console.ReadLine();
+
+            switch (response)
+            {
+                case "1":
+                    new Juego();
+                    break;
+                default:
+                    
+                    break;
+            }
         }
     }
 }
